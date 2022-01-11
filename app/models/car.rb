5d3b664e2df94a82730ef53b2e4efe149e0a386f
Julio AG <julio.antequera.galiano@gmail.com>
2022-01-11 13:24:36 +0100

    Add state machine, method and test that will trigger the warning
    rake
    Running via Spring preloader in process 58496
    Run options: --seed 8440
    
    # Running:
    
    /Users/julio/.rbenv/versions/2.7.2/lib/ruby/gems/2.7.0/gems/state_machines-0.5.0/lib/state_machines/event.rb:224: warning: Using the last argument as keyword parameters is deprecated; maybe ** should be added to the call
    /Users/julio/code/state_machine_test/app/models/car.rb:10: warning: The called method `ignite' is defined here
    .
    
    Finished in 0.132930s, 7.5228 runs/s, 22.5683 assertions/s.
    1 runs, 3 assertions, 0 failures, 0 errors, 0 skips

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,14 @@
 class Car < ApplicationRecord
   belongs_to :user, optional: true
+
+  state_machine :state, initial: :parked do
+    event :ignite do
+      transition parked: :idling
+    end
+  end
+
+  def ignite(user:)
+    self.update(user: user)
+    super()
+  end
 end
--- test/models/car_test.rb
+++ test/models/car_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class CarTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "ruby 2.7 kwargs error" do
+    car = Car.create
+    assert_equal car.state, 'parked'
+
+    driver = User.create
+    # should issue a warning
+    car.ignite!(user: driver)
+
+    assert_equal car.state, 'idling'
+    assert_equal car.user, driver
+  end
 end
